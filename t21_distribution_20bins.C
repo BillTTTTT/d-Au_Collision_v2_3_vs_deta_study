c5ae473e7748aab5b4eee787bff7694331bfc180
BillTTTTT <air@engr2-2-75-220-dhcp.int.colorado.edu>
2016-03-16 13:12:46 -0600

    added labels, divided into 4 distributions of dphi, also contain the fit lines

--- t21_distribution_20bins.C
+++ t21_distribution_20bins.C
@@ -27,6 +27,7 @@
 #include "TH2.h"
 #include "TH3.h"
 #include "TProfile.h"
+#include "TF1.h"
 using namespace std;
 
 //------------------------
@@ -58,12 +59,15 @@ struct particle
 const int NUCL = 199;
 
 //Number of bins in histogram
-const int nob = 20;
+const int nob = 4;
+
+const int nog = 4;
 
 vector<particle> particles;
 int n = 0;
 
 vector<TH1F*> dhis2;
+vector<TF1*>  fit;
 
 int   ct[20];
 float v12[20];
@@ -188,7 +192,7 @@ void processEvent()
 
  void parseampt()
 {
-    for(int i=0; i<5; i++)
+    for(int i=0; i<1; i++)
     {
         //Read in data file
         ifstream dataFile;
@@ -281,19 +285,27 @@ void processEvent()
 
 void t21_distribution_20bins()
 {
-    for(int i=0; i<20; i++)
+    //Define 20 histograms
+    for(int i=0; i<nog; i++)
     {
         dhis2.push_back(new TH1F(Form("d_%i",i),  Form("v2: dphi in range of [%f,%f)",i*0.1,(i+1)*0.1), 50, -0.5*TMath::Pi(), 1.5*TMath::Pi()));
     }
 
+    //Define the fit graphs
+    for(int i=0; i<nog; i++)
+    {
+        fit.push_back(new TF1(Form("f_%i",i),"[0]*(1 + 2*[1]*cos(x) + 2*[2]*cos(2*x) + 2*[3]*cos(3*x))", -0.5*TMath::Pi(), 1.5*TMath::Pi()));
+    }
+
     parseampt();
     
-    TCanvas *c1 = new TCanvas("c1","c1",2000,1600);
-    c1->Divide(5,4);
+    TCanvas *c1 = new TCanvas("c1","c1",1600,1200);
+    c1->Divide(2,2);
 
     TLatex latex;
 
-    for(int i=0; i<20; i++)
+    //Show the number of v12, v22, v32
+    for(int i=0; i<nog; i++)
     {
         v12[i] = v12[i] / ct[i];
         char strv12[10];
@@ -305,15 +317,15 @@ void t21_distribution_20bins()
         char strv32[10];
         sprintf(strv32, "%f", v32[i]);
 
-        int markx = i%5;
-        int marky = i/5;
-        float x1 = 0.040 + 0.2 * markx;
-        float x2 = 0.075 + 0.2 * markx;
-        float y1 = 0.95 - 0.25 * marky;
-        float y2 = 0.94 - 0.25 * marky;
-        float y3 = 0.93 - 0.25 * marky;
+        int markx = i%2;
+        int marky = i/2;
+        float x1 = 0.08 + 0.5 * markx;
+        float x2 = 0.15 + 0.5 * markx;
+        float y1 = 0.90 - 0.5 * marky;
+        float y2 = 0.86 - 0.5 * marky;
+        float y3 = 0.82 - 0.5 * marky;
         
-        latex.SetTextSize(0.01);
+        latex.SetTextSize(0.02);
         latex.SetTextAlign(11);
         latex.DrawLatex(x1, y1,"#LTcos(1#Delta#phi)#GT = ");
         latex.DrawLatex(x2, y1,strv12);
@@ -323,14 +335,54 @@ void t21_distribution_20bins()
         latex.DrawLatex(x2, y3,strv32);
     }
 
-    for(int i=0; i<20; i++)
+    //Draw graphs
+    for(int i=0; i<nog; i++)
     {   
         c1->cd(i+1);
         dhis2[i]->Draw();
+
+        //Fit the graphs
+        dhis2[i]->Fit(fit[i],"R");
+
+        //Get the parameters from the function
+        fit[i] = (TF1*)dhis2[i]->GetFunction(Form("f_%i",i));
+        float c = fit[i]->GetParameter(0);
+        float x = fit[i]->GetParameter(1);
+        float y = fit[i]->GetParameter(2);
+        float z = fit[i]->GetParameter(3);
+
+        //Draw only v12 fit function
+        TF1 *f1 = new TF1("f1","[0]*(1 + 2*[1]*cos(x))",-0.5*TMath::Pi(), 1.5*TMath::Pi()); 
+        f1->SetParameter(0,c);
+        f1->SetParameter(1,x);
+        f1->Draw("same");
+        f1->SetLineColor(kGreen);
+
+        //Draw only v22 fit function
+        TF1 *f2 = new TF1("f2","[0]*(1 + 2*[1]*cos(2*x))",-0.5*TMath::Pi(), 1.5*TMath::Pi()); 
+        f2->SetParameter(0,c);
+        f2->SetParameter(1,y);
+        f2->Draw("same");
+        f2->SetLineColor(kBlack);
+
+        //Draw only v32 fit function
+        TF1 *f3 = new TF1("f3","[0]*(1 + 2*[1]*cos(3*x))",-0.5*TMath::Pi(), 1.5*TMath::Pi()); 
+        f3->SetParameter(0,c);
+        f3->SetParameter(1,z);
+        f3->Draw("same");
+        f3->SetLineColor(kOrange);
+
         gStyle->SetOptTitle(0);
         gStyle->SetOptStat(0);
         gStyle->SetOptFit(111);
         dhis2[i]->SetXTitle("#Delta#phi");
+
+        TLegend *leg = new TLegend(0.65,0.15,0.85,0.35);
+        leg->AddEntry(fit[i],"fit line","L");
+        leg->AddEntry(f1,"cos(1#Delta#phi)","L");
+        leg->AddEntry(f2,"cos(2#Delta#phi)","L");
+        leg->AddEntry(f3,"cos(3#Delta#phi)","L");
+        leg->Draw("same");
     }
 
     c1->Print("AMPT_20dis.pdf");
